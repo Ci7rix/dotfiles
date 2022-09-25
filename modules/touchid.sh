e86bc5a1c5a7ddc7d1b87f1af8bed694bf083e84
Max Roy <max@unset.ch>
2022-09-25 16:57:40 +0200

    Cleaning scripts

---
+++ modules/touchid.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env zsh
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until `.macos` has finished
+while true; do
+    sudo -n true
+    sleep 60
+    kill -0 "$$" || exit
+done 2>/dev/null &
+
+if ! grep 'pam_tid.so' /etc/pam.d/sudo > /dev/null; then
+    sudo sed -i '' '2i\
+auth       optional       /opt/local/lib/pam/pam_reattach.so \
+auth       sufficient     pam_tid.so
+    ' /etc/pam.d/sudo
+fi
